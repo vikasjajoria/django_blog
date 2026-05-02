b118eba9a9839617d30eb29c5d03566bd78041df
Your Name <your-email@example.com>
2026-05-02 17:37:02 +0530

    blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blogs',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR/ 'media'
---
+++ blogs/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+# Create your models here.
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    class meta:
+        verbose_name_plural = 'categoies'
+
+    def __str__(self):
+        return self.category_name    
+    
+STATUS_CHOIES = (
+    (0, "Draft"),
+    (1, "Published")
+)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=120, unique=True)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) 
+    featured_image = models.ImageField(upload_to='uploads/%y/%m%d') 
+    short_description = models.TextField(max_length=500) 
+    blog_body = models.TextField(max_length=2000)
+    status = models.IntegerField(choices=STATUS_CHOIES, default=0) 
+    is_featured = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
