6f69cddcbf072b4d3593e811e0c3ff90b7e87cca
Your Name <your-email@example.com>
2026-05-04 15:32:42 +0530

    add search features

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 from blogs.models import Blog, Category
+from assignments.models import About
 
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
     normal_posts = Blog.objects.filter(is_featured=False).order_by('-created_at')
+
+    # Fetch about us
+    try:
+        about = About.objects.get()
+    except:    
+        about = None
+
+
+
     context = {
         'featured_posts': featured_posts,
         'posts': normal_posts,
+        'about': about,
     }
     return render(request, 'home.html', context)
     
--- blogs/views.py
+++ blogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Blog, Category
+from django.db.models import Q
 
 # Create your views here.
 def posts_by_category(request, category_id):
@@ -15,3 +16,24 @@ def posts_by_category(request, category_id):
         'category': category,
     }
     return render(request, 'posts_by_category.html', context)
+
+def blogs(request, slug):
+    single_blog = get_object_or_404(Blog, slug=slug)
+    context = {
+        'single_blog': single_blog
+    }
+    return render(request, 'blogs.html', context)
+
+
+
+def search(request):
+    keyword = request.GET.get('keyword')
+
+    blogs = []
+    if keyword:
+        blogs = Blog.objects.filter(Q(title__icontains=keyword)| Q(short_description__icontains=keyword) | Q(blog_body__icontains=keyword),  status=1)
+    context = {
+        'blogs': blogs,
+        'keyword': keyword,
+    }
+    return render(request, 'search.html', context)
