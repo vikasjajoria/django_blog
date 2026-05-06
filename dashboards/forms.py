89011d29fda96e02786524cd47946d89d9d58dbf
Your Name <your-email@example.com>
2026-05-06 16:53:48 +0530

    posts crud operations preform / function

--- dashboards/forms.py
+++ dashboards/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from blogs.models import Category
+from blogs.models import Category, Blog
 
 
 
@@ -7,3 +7,9 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = '__all__'
+
+
+class BlogPostForm(forms.ModelForm):
+    class Meta:
+        model = Blog
+        fields = ('title','category','featured_image','short_description','blog_body','status','is_featured')
